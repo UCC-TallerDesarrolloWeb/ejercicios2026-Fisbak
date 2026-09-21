3859b6591f846aa359d3445724df8339d1a898cb
Franco <francotorres0209@gmail.com>
2026-09-21 09:47:32 -0300

    feat: add dialog to cabezal-cerrado

--- 3-Js/tienda.js
+++ 3-Js/tienda.js
@@ -61,3 +61,19 @@ const productos = [
     imagen: "protectores-manos.webp",
   },
 ];
+
+/**
+ * Mostrar un modal con el detalle de Producto
+ * @method mostrarModal
+ */
+mostrarModal = () => {
+  document.getElementById("modal").style.display = "block";
+}
+
+/**
+ * Ocultar un modal con el detalle de Producto
+ * @method cerrarModal
+ */
+cerrarModal = () => {
+  document.getElementById("modal").style.display = "none";
+}
